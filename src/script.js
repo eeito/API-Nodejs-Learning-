4eca1a4290ba394a7633654a31e58ebdfd24a9c9
eeito <heitorhfpb@gmail.com>
2026-01-13 13:07:37 -0300

    fizemos mibombos com cores diferentes

---
+++ src/script.js
@@ -0,0 +1,12 @@
+function mibombo() {
+  //alert("Mi bombo")
+  let mibombolabel = document.getElementById("mibombolabel")
+  
+  if (mibombolabel.innerText == "bomboclat") {
+    mibombolabel.innerText = "Mibombo"
+  }
+  else {
+    mibombolabel.innerText = "bomboclat"
+  }
+  
+}
